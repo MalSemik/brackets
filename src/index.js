00f7218392bead341ce7a06416bfeb90bf0e8022
ms <malg.sem@gmail.com>
2020-03-17 22:42:13 +0100

    fix: adjust for the same opening and closing brackets

--- src/index.js
+++ src/index.js
@@ -1,43 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-    // let i = 0;
-    // let flag = false;
-    // let configLen = bracketsConfig.length;
-    // while (i < configLen) {
-    //     flag = false;
-    //     let bracketsJoin = bracketsConfig[i].join('');
-    //     if (str.includes(bracketsJoin)) {
-    //         str = str.replace(bracketsJoin, '')
-    //         flag = true;
-    //     }
-    //     if (flag) {
-    //         i = 0;
-    //     } else {
-    //         i++;
-    //     }
-    // }
-    // if (str.length) {
-    //     return false;
-    // }
-
-    // return true;
-    if (str.length % 2 != 0) {
-        return false;
-    }
     let arr = [];
     let dict = {};
     for (let i = 0; i < bracketsConfig.length; i++) {
         dict[bracketsConfig[i][1]] = bracketsConfig[i][0];
     }
+
     for (let j = 0; j < str.length; j++) {
-        if (str[j] in dict) {
-            let last = arr.pop();
-            if (last != dict[str[j]]) {
-                return false;
+        if (str[j] in dict && str[j] == dict[str[j]]) {
+            if (str[j] == arr[arr.length - 1]) {
+                arr.pop();
+            } else {
+                arr.push(str[j]);
             }
         } else {
-            arr.push(str[j]);
+            if (str[j] in dict) {
+                let last = arr.pop();
+                if (last != dict[str[j]]) {
+                    return false;
+                }
+            } else {
+                arr.push(str[j]);
+            }
         }
     }
-
+    if (arr.length != 0) {
+        return false;
+    }
     return true
 }
